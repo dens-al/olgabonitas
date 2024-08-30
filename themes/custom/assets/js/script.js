766a30681ab93c13da162d4238f424711c69e982
dens-al <d.almuhametov@gmail.com>
2024-08-30 14:28:27 +0100

    first commit

---
+++ themes/custom/assets/js/script.js
@@ -0,0 +1,6 @@
+// themes/custom/assets/js/script.js
+console.log("scripts loaded");
+
+const yearDate = new Date().getFullYear().toString();
+document.querySelector(".year").innerText = yearDate;
+
